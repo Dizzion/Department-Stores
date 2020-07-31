509834ff5a63e5cb95d40524cf4ec41840d0b572
AShaver43 <gofrodude_197@hotmail.com>
2020-07-31 16:35:40 -0700

    added routing functions for department controller

--- controllers/departments.js
+++ controllers/departments.js
@@ -1,1 +1,79 @@
-// Sams Section
+// Alex's Section
+const Depts = require('../modules/departments')
+const Product = require('../modules/products')
+
+// export all functionality to router
+module.exports = {
+    indexDepts,
+    showDepts,
+    newDepts,
+    addDepts,
+    deleteDepts,
+    editDepts,
+    updateDepts
+}
+// dept index render
+function indexDepts(req, res) {
+    Depts.find({}, (err, allDepts) => {
+        res.render('Departments/index', {
+            Dept: allDepts
+        })
+    })
+}
+// show dept indivdualy
+function showDepts(req, res) {
+    Depts.findById(req.params.id)
+    .populate({path: 'products'})
+    .exec((err, foundDept) => {
+        if(err) {
+            res.send(err)
+        } else {
+            res.render('Departments/show', {
+                Dept: foundDept
+            })
+        }
+    })
+}
+// render the page to add a new dept
+function newDepts(req, res) {
+    res.render('Departments/new')
+}
+//  add a new dept to the database
+function addDept(req, res) {
+    Depts.create(req.body, (err, addedDept) => {
+        res.redirect('/Store/Depts')
+    })
+}
+// delete a dept from the database
+function deleteDepts(req, res) {
+    Depts.findByIdAndRemove(req.params.id, (err, deletedDept) => {
+        if(err) {
+            res.send(err)
+        } else {
+            product.remove({
+                _id: {
+                    $in: deletedDept.products
+                }
+            }, (err, data) => {
+                res.redirect('/Store/Depts')
+            })
+        }
+    })
+}
+// edit a Dept in the database
+function editDepts(req, res) {
+    Depts.findById(req.params.id, (err, foundDept) => {
+        res.render(
+            'Departments/edit',
+            {
+                Depts: foundDept
+            }
+        )
+    })
+}
+// update a Dept in the database from the updated info
+function updateDepts(req, res) {
+    Depts.findByIdAndUpdate(req.params.id, req.body, (err, updatedDept) => {
+        res.redirect('/Store/Depts')
+    })
+}
